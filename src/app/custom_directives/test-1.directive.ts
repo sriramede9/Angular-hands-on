2436f256703cbc69577364ae8071a1ee5dab6930
sriramede9 <only4programming03@gmail.com>
2020-03-29 19:46:55 +0100

    #@HOSTBINDING , bgColor,usage

--- src/app/custom_directives/test-1.directive.ts
+++ src/app/custom_directives/test-1.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener } from "@angular/core";
+import { Directive, HostListener, HostBinding } from "@angular/core";
 import { ElementRef } from "@angular/core";
 import { Renderer2 } from "@angular/core";
 import { OnInit } from "@angular/core";
@@ -9,25 +9,29 @@ import { OnInit } from "@angular/core";
 export class Test1Directive implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
+  @HostBinding("style.backgroundColor") bgColor: string;
+
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-
-    this.renderer.setStyle(this.elementRef.nativeElement, "color", "green");
+    //this.renderer.setStyle(this.elementRef.nativeElement, "color", "green");
+    //this.bgColor="green";
   }
 
   @HostListener("mouseenter") mouseenter(eventData: Event) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      "background-color",
-      "lightgrey"
-    );
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   "background-color",
+    //   "lightgrey"
+    // );
+    this.bgColor = "lightgrey";
   }
   @HostListener("mouseleave") mouseleave(eventData: Event) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      "background-color",
-      "transparent"
-    );
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   "background-color",
+    //   "transparent"
+    // );
+    this.bgColor = "transparent";
   }
 }
